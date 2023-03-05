673756feb9d6790940f570b07aa99ab54d6aadd7
Ecem Gokdogan <ecemgo@hotmail.com>
2023-03-05 14:03:50 +0300

    Add files via upload

---
+++ key.js
@@ -0,0 +1,8 @@
+//* First, you need to get your own OMDb API key to run this app properly.
+//* For that, you have to sign up for an account at http://www.omdbapi.com/apikey.aspx.
+//* You'll be sent a verification link containing your API key via email.
+//* Then, you type that key below.
+
+// API KEY
+
+key = "";
