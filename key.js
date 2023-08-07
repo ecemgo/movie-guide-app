c332b13162b9b1fb5a3a0405e1c626598862fad5
Ecem Gokdogan <ecemgo@hotmail.com>
2023-08-07 23:23:48 +0300

    deleted js file

--- key.js
+++
@@ -1,7 +0,0 @@
-//* First, you need to get your own OMDb API key to run this code properly.
-//* For that, you have to sign up for an account at http://www.omdbapi.com/apikey.aspx.
-//* You'll be sent a verification link containing your API key via email.
-//* Then, you'll need to type that key below.
-
-// Sample API KEY - "fc1fef96"
-key = "fc1fef96";
